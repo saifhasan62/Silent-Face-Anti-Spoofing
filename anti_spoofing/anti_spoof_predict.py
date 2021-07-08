a4fb66a40a72e00c5767bfe793f46c94655b3fa7
Saif Hasan <69043600+saifhasan62@users.noreply.github.com>
2021-07-08 13:40:43 +0530

    Default value Cuda:0
    Default value Cuda:0 in line 60

--- anti_spoofing/anti_spoof_predict.py
+++ anti_spoofing/anti_spoof_predict.py
@@ -57,7 +57,7 @@ class Detection:
 class AntiSpoofPredict(Detection):
     def __init__(self, device_id):
         super(AntiSpoofPredict, self).__init__()
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 #         self.device = torch.device("cuda:{}".format(device_id)
 #                                    if torch.cuda.is_available() else "cpu")
 
