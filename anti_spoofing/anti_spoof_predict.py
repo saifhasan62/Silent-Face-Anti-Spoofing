131c575aa7b12baae376fd6e7722db9c7b76a755
Saif Hasan <69043600+saifhasan62@users.noreply.github.com>
2021-07-08 22:09:56 +0530

    Removed logs after debugging

--- anti_spoofing/anti_spoof_predict.py
+++ anti_spoofing/anti_spoof_predict.py
@@ -91,9 +91,9 @@ class AntiSpoofPredict(Detection):
         ])
         img = test_transform(img)
 #         log.info(img)
-        log.info('step 1 completed')
+#         log.info('step 1 completed')
         img = img.unsqueeze(0).to(self.device)
-        log.info('step 2 started')
+#         log.info('step 2 started')
 #         log.info(img)
 #         self._load_model(model_path)
         
